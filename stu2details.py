2ae381004ed0a8762a21980a8c42a41d6371309c
Ratna <ratnavk74@gmail.com>
2025-11-20 14:06:41 +0530

    Create stu2details.py

---
+++ stu2details.py
@@ -0,0 +1,11 @@
+import sys
+if len(sys.argv) ! =3:
+    print("Using : python stu2details.py <name> <roll>")
+    sys.exit(1)
+script_name = sys.argv[0]
+name = sys.argv[1]
+rollno = sys.argv[2]
+
+print("script Name:",script_name)
+print("Student Name:",name)
+print("Roll No:",rollno)
